cb25fba995a58e9019ca689bfd63d0c0f177aa4d
huyqueng <huyqueng@gmail.com>
2025-11-07 10:08:52 +0700

    config mongoose, init schema

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    // MongooseModule.forRoot('mongodb+srv://huyqueng_db_user:Vs8eYYwCrgG3nnDw@cluster1.mob2bsp.mongodb.net'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+      inject: [ConfigService],
+    }),
+  
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,35 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+
+export enum UserRole {
+  USER = 'user',
+  ADMIN = 'admin'
+} 
+
+@Schema()
+export class User {
+  @Prop({ required: true, unique: true, trim: true, lowercase: true })
+  email: string;
+
+  @Prop({ required: true, minlength: 6 })
+  password: string;
+
+  @Prop({ enum: UserRole, default: UserRole.USER })
+  role: UserRole;
+
+  @Prop({ required: true, trim: true })
+  fullName: string;
+
+  @Prop({ trim: true, default: '' })
+  avatarUrl: string;
+
+  @Prop({ required: true   })
+  createdAt: Date;
+
+  @Prop()
+  updatedAt: Date;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
